3b7b4a11f574163ad47e15a6ecd147d82f6b0c8b
NetroScript <noreply@enostrion.com>
2023-06-18 23:58:25 +0200

    Add initial README + format source file

---
+++ README.md
@@ -0,0 +1,44 @@
+# GPS Path Average Distance
+
+This Rust application is designed to compute the average distance between a reference GPS path and one or more additional GPS tracks. The calculation is based on determining the closest point on the reference path for each point on the track(s) being compared.
+
+The application provides two output values: an average distance "*in time*"* and an "*location-dependent*" average distance. The rationale behind this distinction is as follows:
+
+When sampling points along the track(s) to compare against the reference, it is possible to encounter multiple points at the same location if a person remains stationary for a period of time. Consequently, the longer the stationary period, the more the distance measurement becomes skewed towards that particular location. However, the focus may be on understanding the actual differences in paths, irrespective of movement speed or time. To address this, the algorithm simplifies the track using the Douglas-Peucker algorithm prior to calculating the average distance. This simplification process retains only points corresponding to changes in the path's direction, resulting in a simplified path. The average distance is then computed based on this simplified representation, which more accurately reflects the average distance between the paths, independent of movement speed or time.
+
+## Installation / Building from Source
+
+1. Install Rust and Cargo from [https://www.rust-lang.org](https://www.rust-lang.org).
+2. Clone the repository or download the source code.
+3. Open a terminal and navigate to the project directory.
+4. Run the command `cargo build --release` to compile the application.
+
+
+## Usage
+
+This assumes you have a compiled version of the application.
+
+The application takes input in the form of GPX files (.gpx) for both the reference path and the tracks to compare. Here's how to use it:
+
+```shell
+gps-path-average-distance [OPTIONS] --reference <REFERENCE> --track <TRACK>
+```
+
+## Options
+
+Options
+* `-r, --reference <REFERENCE>`: File path to the .gpx file containing the reference path.
+* `-t, --track <TRACK>`: File path(s) to the .gpx file(s) containing the track(s) to compare. Separate multiple paths with a comma.
+* `-d, --debug`: Turn on debugging information.
+* `-s, --simplify_epsilon <EPSILON>`: Custom epsilon value for simplifying the reference path. Default is 0.00001.
+    * This value is used as the epsilon in the Douglas-Peucker algorithm for simplifying the current path. The bigger the value, the more simplified the path will be. The reference "space" is in latitude and longitude, for this reason the value is quite small by default.
+* `-e, --export_track`: Toggle to also reexport the parsed GPX files as simplified GPX files.
+    * Exported files will be named `<original_file_name>.modified.gpx` and will be placed in the same directory as the original file. No GPX extensions are supported, so you will be left with only track points containing latitude and longitude.
+
+5. Replace `[arguments]` with the appropriate command-line arguments. Here are the available options:
+
+## Example
+
+```shell
+gps-path-average-distance -r ./reference.gpx -t ./track1.gpx,./track2.gpx -d -e
+```
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
-use std::path::PathBuf;
-use std::io::BufReader;
 use std::fs::File;
+use std::io::BufReader;
+use std::path::PathBuf;
 
-use gpx::{read, Waypoint, write};
-use gpx::{Gpx, Track, TrackSegment};
-use geo::{ClosestPoint,Closest,Simplify, LineString};
-use geo::GeodesicDistance;
 use clap::{arg, command, Parser};
-use colored::{Colorize, control};
-
+use colored::{control, Colorize};
+use geo::GeodesicDistance;
+use geo::{Closest, ClosestPoint, LineString, Simplify};
+use gpx::{read, write, Waypoint};
+use gpx::{Gpx, Track, TrackSegment};
 
 #[derive(Parser)]
-#[command(name = "gps-path-average-distance", version = "1.0", author = "NetroScript", about = "This application takes a reference GPS path and compares it to one to many other gps tracks to then calculate the average distance between the two paths. This works by averaging the distance of the closest point on the reference path to the other path.")]
+#[command(
+    name = "gps-path-average-distance",
+    version = "1.0",
+    author = "NetroScript",
+    about = "This application takes a reference GPS path and compares it to one to many other gps tracks to then calculate the average distance between the two paths. This works by averaging the distance of the closest point on the reference path to the other path."
+)]
 struct Cli {
-
     /// File path to a .gpx file containing the reference path
     #[arg(short, long, required = true)]
     reference: PathBuf,
@@ -23,8 +26,8 @@ struct Cli {
     track: Vec<PathBuf>,
 
     /// Turn debugging information on
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
+    #[arg(short, long)]
+    debug: bool,
 
     /// Custom epsilon value to use for simplifying the reference path. This is the maximum distance between two points before they are simplified. Default is 0.0001.
     #[arg(short, long, default_value = "0.00001")]
@@ -45,29 +48,25 @@ macro_rules! debug_print {
     }
 }
 
-
 fn main() {
-
-
     let _ = control::set_virtual_terminal(true);
 
     // Parse the command line arguments
     let matches = Cli::parse();
 
-
     let reference_path: PathBuf = matches.reference;
 
     // Generate a path buffer from the input strings
     let track_paths: Vec<PathBuf> = matches.track.iter().map(|s| PathBuf::from(s)).collect();
-    //let track_paths: Vec<PathBuf> = matches.get_many::<String>("track").expect("At least one track is required so it can be compared against the reference").map(|s| PathBuf::from(s)).collect();
 
-    let debug_enabled: bool = matches.debug > 0;
-
-
-    debug_print!(debug_enabled, "Debugging is enabled");
-    debug_print!(debug_enabled, "Reference path: {:?}", reference_path);
-    debug_print!(debug_enabled, "Track paths: {:?}", track_paths);
-    debug_print!(debug_enabled, "Simplify epsilon: {}", matches.simplify_epsilon);
+    debug_print!(matches.debug, "Debugging is enabled");
+    debug_print!(matches.debug, "Reference path: {:?}", reference_path);
+    debug_print!(matches.debug, "Track paths: {:?}", track_paths);
+    debug_print!(
+        matches.debug,
+        "Simplify epsilon: {}",
+        matches.simplify_epsilon
+    );
 
     // Check that all passed paths exist and are files
     if !reference_path.exists() {
@@ -105,28 +104,53 @@ fn main() {
         track_gpxs.push(track_gpx);
     }
 
-    // Get the first track of the reference path
-    let reference_track: &Track = &reference_gpx.tracks[0];
+    // Check that the reference path has at least one track
+    let reference_track: Track = if reference_gpx.tracks.len() > 0 {
+        // Get the first track of the reference path
+        reference_gpx.tracks[0].clone()
+    }
+    // Check if the reference path has any waypoints
+    // If so create a Track with a single TrackSegment containing all the waypoints
+    else if reference_gpx.waypoints.len() > 0 {
+        println!("The reference path does not contain any tracks, but it does contain waypoints. Creating a track from the waypoints");
+        let mut track: Track = Track::default();
+        let mut track_segment: TrackSegment = TrackSegment::default();
+        track_segment.points = reference_gpx.waypoints.clone();
+        track.segments.push(track_segment);
+        track.clone()
+    } else {
+        // No waypoints or tracks so we exit the program
+        println!("The reference path does not contain any tracks or waypoints");
+        return;
+    };
 
     // Get the total number of tracks by iterating all the track GPXs and summing the number of tracks
     let total_tracks: usize = track_gpxs.iter().map(|gpx| gpx.tracks.len()).sum();
-    
-    println!("Calculating average distance between reference path ({}) and {} track(s)... ", reference_track.name.as_ref().unwrap_or(&"-- Unnamed --".to_string()).italic(), total_tracks);
+
+    println!(
+        "Calculating average distance between reference path ({}) and {} track(s)... ",
+        reference_track
+            .name
+            .as_ref()
+            .unwrap_or(&"-- Unnamed --".to_string())
+            .italic(),
+        total_tracks
+    );
 
     // Keep track of the current index of a track
     let mut track_index: usize = 0;
 
-
-
     // Iterate every track now
-    for (gpx_index,track_gpx) in track_gpxs.iter().enumerate() {
-
+    for (gpx_index, track_gpx) in track_gpxs.iter().enumerate() {
         // Create a copy of the gpx file so we can modify it and  reexport it if needed
         let mut track_gpx_copy: Gpx = track_gpx.clone();
 
-
         for (cur_track_index, track) in track_gpx.tracks.iter().enumerate() {
-            println!("Track {}: {}", track_index + 1, track.name.as_ref().unwrap_or(&"-- Unnamed --".to_string()));
+            println!(
+                "Track {}: {}",
+                track_index + 1,
+                track.name.as_ref().unwrap_or(&"-- Unnamed --".to_string())
+            );
 
             let mut total_distance: f64 = 0.0;
             let mut total_points: usize = 0;
@@ -140,71 +164,105 @@ fn main() {
 
             // Iterate every segment of the track
             track.segments.iter().for_each(|segment| {
-
                 // Inline function to calculate the total distance of a segment
-                let calculate_segment_distance = |line_string: &LineString, distance: &mut f64, points: &mut usize| {
-                    // For every point in the segment, find the closest point on the reference path
-                    line_string.points().for_each(|point| {
-                        
-                        // By default, set the closest distance to infinity
-                        let mut closest_distance: f64 = f64::INFINITY;
+                let calculate_segment_distance =
+                    |line_string: &LineString, distance: &mut f64, points: &mut usize| {
+                        // For every point in the segment, find the closest point on the reference path
+                        line_string.points().for_each(|point| {
+                            // By default, set the closest distance to infinity
+                            let mut closest_distance: f64 = f64::INFINITY;
 
-                        // Find the distance to the closest point on the reference path
-                        reference_track.segments.iter().for_each(|reference_segment| {
+                            // Find the distance to the closest point on the reference path
+                            reference_track
+                                .segments
+                                .iter()
+                                .for_each(|reference_segment| {
+                                    let closest =
+                                        reference_segment.linestring().closest_point(&point);
+                                    let distance = match closest {
+                                        Closest::Intersection(p) => p.geodesic_distance(&point),
+                                        Closest::Indeterminate => f64::INFINITY,
+                                        Closest::SinglePoint(p) => p.geodesic_distance(&point),
+                                    };
 
-                            let closest = reference_segment.linestring().closest_point(&point);
-                            let distance = match closest {
-                                Closest::Intersection(p) => p.geodesic_distance(&point),
-                                Closest::Indeterminate => f64::INFINITY,
-                                Closest::SinglePoint(p) => p.geodesic_distance(&point),
-                            };
+                                    if distance < closest_distance {
+                                        closest_distance = distance;
+                                    }
+                                });
 
-                            if distance < closest_distance {
-                                closest_distance = distance;
-                            }
+                            // Add the closest distance to the total distance
+                            *distance += closest_distance;
+                            *points += 1;
                         });
-
-                        // Add the closest distance to the total distance
-                        *distance += closest_distance;
-                        *points += 1;
-                    });
-                };
+                    };
 
                 // Calculate the average distance of the segment just based on the points
-                calculate_segment_distance(&segment.linestring(), &mut total_distance, &mut total_points);
-                // Also create a simplified version of the linestring, where points that are close to each other are removed to have a more accurate average distance
-                let simplified_linestring: LineString = segment.linestring().simplify(&matches.simplify_epsilon);
+                calculate_segment_distance(
+                    &segment.linestring(),
+                    &mut total_distance,
+                    &mut total_points,
+                );
+                // Also create a simplified version of the linestring, 
+                // where points that are close to each other are removed to have
+                // a more accurate average distance
+                let simplified_linestring: LineString =
+                    segment.linestring().simplify(&matches.simplify_epsilon);
 
-                debug_print!(debug_enabled, "Original points: {:?} - Simplified points: {:?}", segment.linestring().points().count(), simplified_linestring.points().count());
+                debug_print!(
+                    matches.debug,
+                    "Original points: {:?} - Simplified points: {:?}",
+                    segment.linestring().points().count(),
+                    simplified_linestring.points().count()
+                );
 
                 // If we want to export the simplified linestring, do it now
                 if matches.export_track {
                     let mut simplified_segment: TrackSegment = TrackSegment::new();
-                    simplified_segment.points = simplified_linestring.points().map(|point| Waypoint::new(point)).collect();
-                    track_gpx_copy.tracks[cur_track_index].segments.push(simplified_segment);
+                    simplified_segment.points = simplified_linestring
+                        .points()
+                        .map(|point| Waypoint::new(point))
+                        .collect();
+                    track_gpx_copy.tracks[cur_track_index]
+                        .segments
+                        .push(simplified_segment);
                 }
 
                 // Calculate the total distance of the simplified linestring
-                calculate_segment_distance(&simplified_linestring, &mut total_distance_simplified, &mut total_points_simplified);
+                calculate_segment_distance(
+                    &simplified_linestring,
+                    &mut total_distance_simplified,
+                    &mut total_points_simplified,
+                );
             });
 
-            println!("Average distance (in time): {} (counting every point)", (format!("{:.3}m", (total_distance / total_points as f64))).cyan().bold());
-            println!("Average distance (location dependant): {} (counting only simplified points)", (format!("{:.3}m", (total_distance_simplified / total_points_simplified as f64))).yellow().bold());
+            println!(
+                "Average distance (in time): {} (counting every point)",
+                (format!("{:.3}m", (total_distance / total_points as f64)))
+                    .cyan()
+                    .bold()
+            );
+            println!(
+                "Average distance (location dependent): {} (counting only simplified points)",
+                (format!(
+                    "{:.3}m",
+                    (total_distance_simplified / total_points_simplified as f64)
+                ))
+                .yellow()
+                .bold()
+            );
 
-             
             track_index += 1;
         }
-
 
         // If we want to reexport the GPX files, do it now by writing the modified GPX file to the same path, adding .modified before the extension
         if matches.export_track {
             let track_path = track_paths[gpx_index].clone();
             let mut modified_path = track_path.clone();
             modified_path.set_extension("modified.gpx");
-            let track_file = File::create(&modified_path).expect("Failed to create modified track file");
+            let track_file =
+                File::create(&modified_path).expect("Failed to create modified track file");
             write(&track_gpx_copy, track_file).expect("Failed to write modified track file");
             println!("Exported modified track file to {:?}", &modified_path);
         }
     }
-
 }
